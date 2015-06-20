d62ca6a973f1e5f1cf6b7534856fb35e7d72682a
yenbohuang <yenbohuang@hotmail.com>
2015-06-21 00:46:30 +0800

    add answer for "FrogJmp"

---
+++ src/main/java/org/yenbo/onlinecontest/sourceb/FrogJmp.java
@@ -0,0 +1,26 @@
+package org.yenbo.onlinecontest.sourceb;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FrogJmp {
+
+	@Test
+	public void test1() {
+		Assert.assertEquals(3, new Solution().solution(10, 85, 30));
+	}
+	
+	class Solution {
+	    public int solution(int X, int Y, int D) {
+	        
+	    	int steps = (Y - X) / D;
+	    	
+	    	if (X + steps * D < Y) {
+	    		return steps + 1;
+	    	} else {
+	    		return steps;
+	    	}
+	    }
+	}
+}
